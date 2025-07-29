4f12053b9a2305d1fc8953175618f35efd9b75c7
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 23:23:39 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,3 @@
-output "s3_bucket"        { value = module.s3.bucket_id }
-output "sns_topic_arn"    { value = module.sns.topic_arn }
-output "cloudtrail_arn"   { value = module.cloudtrail.trail_arn }
-#output "log_group_arn"    { value = module.cloudwatch.log_group_arn }
+output "cloud_watch_logs_group_arn" {
+value  = module.cloud_watch.cloud_watch_logs_group_arn
+}
