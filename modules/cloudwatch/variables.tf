74c883f1f65483b1cc3df2dc62aa075f2fe24820
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 20:40:51 +0530

    Create variables.tf

---
+++ modules/cloudwatch/variables.tf
@@ -0,0 +1,6 @@
+variable "log_group_name" { type = string }
+variable "retention" { type = number }
+variable "metric_name" { type = string }
+variable "metric_namespace" { type = string }
+variable "alarm_name" { type = string }
+variable "sns_topic_arn" { type = string }
