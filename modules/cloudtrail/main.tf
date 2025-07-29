df9bd34bb04f508e75923f760a5d128ea2b94063
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 23:11:41 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -2,6 +2,7 @@ data "aws_caller_identity" "current" {}
 
 locals {
   log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}"
+
 }
 
 resource "aws_iam_role" "cloudtrail_cwlogs" {
