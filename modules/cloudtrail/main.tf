c104cb5ffa1588b961100243a9372212b35c884f
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 23:19:01 +0530

    Update main.tf

--- modules/cloudtrail/main.tf
+++ modules/cloudtrail/main.tf
@@ -1,48 +1,21 @@
-data "aws_caller_identity" "current" {}
-
-locals {
-  log_group_arn = "arn:aws:logs:${var.region}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}"
-
-}
-
-resource "aws_iam_role" "cloudtrail_cwlogs" {
-  name = "${var.trail_name}-cloudwatch-logs"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "cloudtrail.amazonaws.com" },
-      Action   = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "cloudtrail_cwlogs" {
-  name = "${var.trail_name}-cwlogs"
-  role = aws_iam_role.cloudtrail_cwlogs.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "${local.log_group_arn}:log-stream:*"
-      }
-    ]
-  })
-}
-
-resource "aws_cloudtrail" "this" {
-  name                          = var.trail_name
-  s3_bucket_name                = var.s3_bucket
+resource "aws_cloudtrail" "main" {
+  name                          = var.cloud_trail_name
+  s3_bucket_name                = var.cloud_trail_bucket_name
   include_global_service_events = true
   is_multi_region_trail         = true
-  enable_logging                = true
-  cloud_watch_logs_group_arn    = local.log_group_arn   
-  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_cwlogs.arn
+  enable_log_file_validation    = true
 
-  depends_on = [aws_iam_role_policy.cloudtrail_cwlogs]
+  cloud_watch_logs_group_arn    = "${var.cloud_watch_logs_group_arn}:*"
+  cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail_role.arn
+
+  event_selector {
+    read_write_type              = "All"
+    include_management_events   = true
+  }
+
+  tags = var.project_tag
+
+  depends_on = [
+    aws_iam_role_policy_attachment.cloudtrail_policy_attachment,
+  ]
 }
