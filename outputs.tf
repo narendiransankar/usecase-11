d81983eef168ee97f14c82bd31f12708c5a27e69
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 20:43:01 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "s3_bucket"        { value = module.s3.bucket_id }
+output "sns_topic_arn"    { value = module.sns.topic_arn }
+output "cloudtrail_arn"   { value = module.cloudtrail.trail_arn }
+output "log_group_arn"    { value = module.cloudwatch.log_group_arn }
