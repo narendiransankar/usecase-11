23da4ed0c778dc622d58e373ba339798fcf2ea57
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 22:19:00 +0530

    Update variables.tf

--- modules/cloudtrail/variables.tf
+++ modules/cloudtrail/variables.tf
@@ -1,5 +1,5 @@
 
-variable "log_group_depends_on" { type = string }
+#variable "log_group_depends_on" { type = string }
 variable "trail_name"     { type = string }
 variable "s3_bucket"      { type = string }
 variable "log_group_name" { type = string }
