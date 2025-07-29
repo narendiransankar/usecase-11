a5a406cb02c5889686c13716355f21c124d03b8c
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 23:21:50 +0530

    Update variables.tf

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,2 +1,8 @@
-variable "bucket_name" { type = string }
-variable "tags" { type = map(string) }
+variable "cloud_trail_bucket_name" {
+description = "The cloud_trail_bucket_name for storing the cloud Trail logs"
+type = string 
+}
+variable "project_tag" {
+description = "The project_tag for this project"
+type = map(string)
+}
