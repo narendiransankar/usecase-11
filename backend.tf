3d5a00e6bc7d1696c42f4396d1c2ee8a4d09b7f7
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 20:56:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "new-state-temp"
     key            = "usecase-11-new/terraform.tfstate"
     region         = "ap-south-1"                
-    use_lockfile = true
+    #use_lockfile = true
 
   }
 }
