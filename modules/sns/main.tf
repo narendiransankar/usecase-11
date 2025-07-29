db78a6a05c6dfd557e544bf0542262a1f964b26e
narendiransankar <narendiransankar1699@gmail.com>
2025-07-29 23:22:11 +0530

    Update main.tf

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,9 +1,9 @@
-resource "aws_sns_topic" "this" {
-  name = var.topic_name
+resource "aws_sns_topic" "cloudtrail_topic" {
+  name = "cloudtrail-sns-topic"
+  tags =var.project_tag
 }
-
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.this.arn
+resource "aws_sns_topic_subscription" "cloudtrail_email_subscription" {
+  topic_arn = aws_sns_topic.cloudtrail_topic.arn
   protocol  = "email"
-  endpoint  = var.email
+  endpoint  = "prakash.velusamy@hcltech.com" 
 }
